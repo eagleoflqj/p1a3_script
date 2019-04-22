566debcbf15254a6df9aa5eeec77e7d20d40b7a1
Liumeo <liumeo@pku.edu.cn>
2019-04-22 10:39:58 +0800

    auto answer 19

--- p1a3_script.js
+++ p1a3_script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         1p3a_script
 // @namespace    https://github.com/eagleoflqj/p1a3_script
-// @version      0.4.18
+// @version      0.4.19
 // @description  方便使用一亩三分地
 // @author       Liumeo
 // @match        https://www.1point3acres.com/bbs/*
@@ -60,7 +60,8 @@
                 '【题目】 地里面经数目最多的是哪家公司？': 'Amazon',
                 '【题目】 回答别人的私信提问还需要消耗我5大米怎么办？': '直接在版面回答，这样大家都能看见',
                 '【题目】 给论坛ios或者安卓手机应用留评价如何获取50大米？': '以上步骤都需要',
-                '【题目】 下面哪类版块，可以拉群，而且不会被警告扣分？':'学友工友、找室友或者版聚本地',
+                '【题目】 下面哪类版块，可以拉群，而且不会被警告扣分？': '学友工友、找室友或者版聚本地',
+                '【题目】 一亩三分地是哪年创立的？': '2009',
             }; //题库
             let dayquestion = jq('#um img[src*=ahome_dayquestion]').parent()[0];
             dayquestion && dayquestion.onclick && (dayquestion.onclick() && 0 || setTimeout(() => {
