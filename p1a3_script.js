2902612badf8799e5362ecdeb7e60e1c7f4a3330
liumeo <liumeo@pku.edu.cn>
2019-05-19 12:49:47 +0800

    auto answer 34

--- p1a3_script.js
+++ p1a3_script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         1p3a_script
 // @namespace    https://github.com/eagleoflqj/p1a3_script
-// @version      0.5.2
+// @version      0.5.3
 // @description  方便使用一亩三分地
 // @author       Liumeo
 // @match        https://www.1point3acres.com/bbs/*
@@ -69,6 +69,7 @@
                 '下面哪个州，没有state income tax?': 'Florida',
                 '下面哪个州，有state income tax': 'Mississippi',
                 '下面哪个州里有Disney World？': 'Florida',
+                '下面哪个州在美国西海岸': 'Washington',
                 '下面哪个专业，不是STEM，OPT没法延期？': '教育学',
                 '下面哪类版块，可以拉群，而且不会被警告扣分？': '学友工友、找室友或者版聚本地',
                 '下面哪所大学所在城市不是波士顿？': 'MIT',
