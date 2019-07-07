21c55a04251241fd9e8aa90104a396e6f3eb09a3
Liumeo <liumeo@pku.edu.cn>
2019-07-07 23:05:30 +0800

    space & const

--- p1a3_script.js
+++ p1a3_script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         1p3a_script
 // @namespace    https://github.com/eagleoflqj/p1a3_script
-// @version      0.6.2
+// @version      0.6.3
 // @description  方便使用一亩三分地
 // @author       Liumeo
 // @match        https://www.1point3acres.com/bbs/*
@@ -14,11 +14,11 @@
 (function () {
     'use strict';
 
-    let jq = jQuery.noConflict();
-    let waitUntilElementLoaded = function (element, retryTimes = 20) { //异步等待元素出现并返回
+    const jq = jQuery.noConflict();
+    const waitUntilElementLoaded = function (element, retryTimes = 20) { // 异步等待元素出现并返回
         return new Promise((resolve, reject) => {
-            let check = (ttl) => {
-                let e = jq(element);
+            const check = (ttl) => {
+                const e = jq(element);
                 if (!e.length && ttl) { // 未加载且未超时
                     setTimeout(check, 1000, ttl - 1);
                 } else {
@@ -28,40 +28,40 @@
             check(retryTimes);
         });
     };
-    //针对不同页面的操作
-    let url = window.location.href;
-    if (url === 'https://www.1point3acres.com/bbs/' || url.search('forum.php') > 0) { //可签到、答题的页面
-        //自动签到
-        let sign = jq('.wp a:contains("签到领奖")')[0];
+    // 针对不同页面的操作
+    const url = window.location.href;
+    if (url === 'https://www.1point3acres.com/bbs/' || url.search('forum.php') > 0) { // 可签到、答题的页面
+        // 自动签到
+        const sign = jq('.wp a:contains("签到领奖")')[0];
         sign && sign.onclick && (sign.onclick() || 1) &&
-            (async () => { //点击签到领奖
-                let qiandao = await waitUntilElementLoaded('#qiandao');
+            (async () => { // 点击签到领奖
+                const qiandao = await waitUntilElementLoaded('#qiandao');
                 if (!qiandao.length) {
                     return;
                 }
-                let faces = qiandao.find('.qdsmilea>li'); //所有表情
-                let selected_face = faces[Math.floor(Math.random() * faces.length)]; //随机选择表情
+                const faces = qiandao.find('.qdsmilea>li'); // 所有表情
+                const selected_face = faces[Math.floor(Math.random() * faces.length)]; // 随机选择表情
                 selected_face.onclick();
-                let todaysay = qiandao.find('#todaysay'); //文字框
-                todaysay.val('今天把论坛帖子介绍给好基友了~'); //快速签到的第一句
-                let button = qiandao.find('button')[0];
+                const todaysay = qiandao.find('#todaysay'); // 文字框
+                todaysay.val('今天把论坛帖子介绍给好基友了~'); // 快速签到的第一句
+                const button = qiandao.find('button')[0];
                 button.onclick();
-            })(); //保证签到对话框加载
-        //签到后自动答题
-        let dayquestion = jq('#um img[src*=ahome_dayquestion]').parent()[0];
+            })(); // 保证签到对话框加载
+        // 签到后自动答题
+        const dayquestion = jq('#um img[src*=ahome_dayquestion]').parent()[0];
         !sign && dayquestion && dayquestion.onclick && (dayquestion.onclick() || 1) &&
             (async () => {
-                let fwin_pop = await waitUntilElementLoaded('#fwin_pop form');
-                let question = fwin_pop.find('font:contains(【题目】)').text().slice(5);
-                let prompt = '尚未收录此题答案。如果您知道答案，请将\n"\n' + question + '\n{您的答案}\n"\n以issue形式提交至https://github.com/eagleoflqj/p1a3_script/issues';
-                let answer = QA[question];
-                if (!answer) { //题库不含此题
+                const fwin_pop = await waitUntilElementLoaded('#fwin_pop form');
+                const question = fwin_pop.find('font:contains(【题目】)').text().slice(5);
+                const prompt = '尚未收录此题答案。如果您知道答案，请将\n"\n' + question + '\n{您的答案}\n"\n以issue形式提交至https://github.com/eagleoflqj/p1a3_script/issues';
+                const answer = QA[question];
+                if (!answer) { // 题库不含此题
                     console.log(prompt);
                     return;
                 }
-                //自动回答
-                let option_list = [];
-                let answer_list = typeof answer === 'string' ? [answer] : answer;
+                // 自动回答
+                const option_list = [];
+                const answer_list = typeof answer === 'string' ? [answer] : answer;
                 // 答案和选项取交集
                 fwin_pop.find('.qs_option').toArray()
                     .forEach(option => answer_list
@@ -76,36 +76,36 @@
                     return;
                 }
                 option_list[0].onclick();
-                let button = fwin_pop.find('button')[0];
-                button.click(); //提交答案
+                const button = fwin_pop.find('button')[0];
+                button.click(); // 提交答案
                 console.log(question + '\n答案为：' + answer);
-            })(); //保证答题对话框加载
+            })(); // 保证答题对话框加载
     }
-    if (url.search('thread') > 0) { //详情页
-        //自动查看学校、三维
-        let elements = jq('.typeoption a:contains(点击查看)');
+    if (url.search('thread') > 0) { // 详情页
+        // 自动查看学校、三维
+        const elements = jq('.typeoption a:contains(点击查看)');
         elements.toArray().forEach(element => element.onclick());
-    } else if (url.search('forum-82-1') > 0 || url.search('forum.php\\?mod=forumdisplay&fid=82') > 0) { //结果汇报列表页
-        //按上次的筛选条件过滤录取结果
-        let search_ids = ['planyr', 'planterm', 'planmajor', 'plandegree', 'planfin', 'result', 'country']; //过滤下拉菜单id
-        let search_button = jq('#searhsort > div.ptm.cl > button'); //搜索按钮
-        if (jq.cookie('searchoption')) { //上次过滤了
-            search_ids.forEach(id => jq('#' + id).val(jq.cookie(id)));//自动填充下拉菜单
-            if (url.search('filter') < 0) { //当前页面没有过滤
-                search_button.click(); //自动过滤
+    } else if (url.search('forum-82-1') > 0 || url.search('forum.php\\?mod=forumdisplay&fid=82') > 0) { // 结果汇报列表页
+        // 按上次的筛选条件过滤录取结果
+        const search_ids = ['planyr', 'planterm', 'planmajor', 'plandegree', 'planfin', 'result', 'country']; // 过滤下拉菜单id
+        const search_button = jq('#searhsort > div.ptm.cl > button'); // 搜索按钮
+        if (jq.cookie('searchoption')) { // 上次过滤了
+            search_ids.forEach(id => jq('#' + id).val(jq.cookie(id)));// 自动填充下拉菜单
+            if (url.search('filter') < 0) { // 当前页面没有过滤
+                search_button.click(); // 自动过滤
                 return;
             }
         }
-        let expire = { expires: 365 }; //cookie有效期
-        search_button.click(() => { //如果不全是默认值，记下当前选项
+        const expire = { expires: 365 }; // cookie有效期
+        search_button.click(() => { // 如果不全是默认值，记下当前选项
             search_ids.some(id => jq('#' + id).val() !== '0') && jq.cookie('searchoption', 1, expire);
             if (jq.cookie('searchoption')) {
                 search_ids.forEach(id => jq.cookie(id, jq('#' + id).val(), expire));
             }
         });
-        //添加重置按钮
-        let reset_button = jq('<button type="button" class="pn pnc"><em>重置</em></button>');
-        reset_button.click(() => { //重置、清cookie
+        // 添加重置按钮
+        const reset_button = jq('<button type="button" class="pn pnc"><em>重置</em></button>');
+        reset_button.click(() => { // 重置、清cookie
             jq.removeCookie('searchoption');
             search_ids.forEach(id => {
                 jq('#' + id).val('0');
@@ -113,8 +113,8 @@
             });
         });
         search_button.after(reset_button);
-        //折叠占空间的提示
-        let img = jq('#forum_rules_82_img')[0];
+        // 折叠占空间的提示
+        const img = jq('#forum_rules_82_img')[0];
         img && img.src.search('collapsed_no') > 0 && img.onclick();
     }
 })();
