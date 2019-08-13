7ddfea2689dd887cfdacd24be111fb167a00e726
liumeo <liumeo@pku.edu.cn>
2019-08-13 14:30:09 +0800

    no need for cookie

--- p1a3_script.js
+++ p1a3_script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         1p3a_script
 // @namespace    https://github.com/eagleoflqj/p1a3_script
-// @version      0.7.0
+// @version      0.7.1
 // @description  方便使用一亩三分地
 // @author       Liumeo
 // @match        https://www.1point3acres.com/bbs/*
@@ -9,7 +9,6 @@
 // @grant        GM_getValue
 // @grant        GM_deleteValue
 // @require      https://cdn.bootcss.com/jquery/3.4.1/jquery.min.js
-// @require      https://cdn.bootcss.com/jquery-cookie/1.4.1/jquery.cookie.js
 // @require      https://raw.githubusercontent.com/eagleoflqj/p1a3_script/master/QA.js
 // ==/UserScript==
 
@@ -30,12 +29,6 @@
             check(retryTimes);
         });
     };
-    (() => {
-        const search_ids = ['planyr', 'planterm', 'planmajor', 'plandegree', 'planfin', 'result', 'country']; // 过滤下拉菜单id
-        const replaceCookie = (c) => jq.cookie(c) && (GM_setValue(c, jq.cookie(c)) || 1) && jq.removeCookie(c); // 替换cookie为本地存储
-        replaceCookie('searchoption');
-        search_ids.forEach(replaceCookie);
-    })();
     // 针对不同页面的操作
     const url = window.location.href;
     if (url === 'https://www.1point3acres.com/bbs/' || url.search('forum.php') > 0) { // 可签到、答题的页面
